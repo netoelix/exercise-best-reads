131edba8a26d4df9f2bd2100e1f8c1ca0bf78ca1
Elix Neto <neto.elix@outlook.com>
2023-09-21 18:37:43 -0300

    Finalizando o requisito 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Book from './components/Book';
 import BookList from './components/BookList';
@@ -8,15 +9,27 @@ const bookIndexEnd = 15;
 const placeHolderList = data.slice(bookIndexStart, bookIndexEnd); // esse código deverá ser excluído após a implementação do requisito 2
 
 function App() {
+  const [index, setIndex] = useState(0);
+  let wishList = {};
+  function handleClick() {
+    if (index + 1 < data.length) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+  function handleAddEvent() {
+    wishList = data[index];
+  }
   return (
     <div className="app">
       <div className="book-selector">
-        <Book bookInfo={ data[0] } showDetails />
+        <Book bookInfo={ data[index] } showDetails />
         <div className="selector-buttons">
-          <button>Adicionar à lista de desejos</button>
+          <button onClick={ handleAddEvent }>Adicionar à lista de desejos</button>
           <button>Adicionar à lista de leitura</button>
           <button>Adicionar à lista de lidos</button>
-          <button>Próximo livro</button>
+          <button onClick={ handleClick }>Próximo livro</button>
         </div>
       </div>
 
